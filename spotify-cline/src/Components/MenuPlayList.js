825912a72c00b41f4cf77ab473519383af6916e9
Santos <realsantosm@gmail.com>
2022-03-24 00:23:40 +0530

    init

---
+++ spotify-cline/src/Components/MenuPlayList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaPlus } from "react-icons/fa";
+import { BsMusicNoteList, BsTrash } from "react-icons/bs";
+import { PlayList } from './PlayList';
+function MenuPlayList() {
+    return <div className='playListContainer'>
+        <div className='nameContainer'>
+            <p>PlayList</p>
+            <i><FaPlus /></i>
+        </div>
+        <div className='playListScroll'>
+            {
+                PlayList && PlayList.map((list) => (
+                 <div className='playList'>
+                <i className='list'><BsMusicNoteList /></i>
+                <p>Music PlayList</p>
+                <i className='trash'><BsTrash /></i>
+            </div>
+                ))
+            }
+        </div>
+    </div>;
+}
+
+export { MenuPlayList };
