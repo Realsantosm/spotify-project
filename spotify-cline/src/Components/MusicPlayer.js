aad98da8abf9e7962faeb2b0d11efd1f4a7b3333
Santos <realsantosm@gmail.com>
2022-03-25 16:04:11 +0530

    Feat : musicPlayer main button

--- spotify-cline/src/Components/MusicPlayer.js
+++ spotify-cline/src/Components/MusicPlayer.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import "../Styles/MusicPlayer.css";
+
+
 
 function MusicPlayer({song, imgSrc}) {
-  return <div>
-      
+  return <div className='musciPlayer'>
+      <div className='songImage'></div>
+      <div className='songAttributes'>
+          <audio src={song} preload="metadata"/>
+          <div className='top'>
+            <div className='left'></div>
+            <div className='middle'></div>
+            <div className='right'></div>
+          </div>
+          <div className='bottom'></div>
+      </div>
   </div>;
 }
 
