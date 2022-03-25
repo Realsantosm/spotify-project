77029aac1c3b9a6f7ea79e5e729e8cffb08aa37c
Santos <realsantosm@gmail.com>
2022-03-25 10:48:33 +0530

    firebase : song added

--- spotify-cline/src/Components/AudioList.js
+++ spotify-cline/src/Components/AudioList.js
@@ -1,48 +1,56 @@
 import React from 'react';
 import { FaHeadphones, FaHeart, FaRegClock, FaRegHeart } from 'react-icons/fa';
 
+import { Songs } from "./Songs";
+
 function AudioList() {
-  return <div className='audioList'>
-            <h2 className='title'>
-                The list <span>12 songs</span>
-            </h2>
+    return <div className='audioList'>
+        <h2 className='title'>
+            The list <span>{`${Songs.length} songs`}</span>
+        </h2>
 
-            <div className='songsContainer'>
-                <div className='songs'>
-                  <div className='count'>#01</div>  
-                     <div className='song'> 
-                        <div className='imgBox'>
-                            <img src="" alt="" />
-                        </div> 
+        <div className='songsContainer'>
 
-                      <div className='section'>
-                          <p className='songName'>
-                              Take me on <span className='spanArtist'>
-                                Artist Name
-                              </span>
-                          </p>
+            {
+                Songs && Songs.map((song, index) => (
+                    <div className='songs' key={song?.id}>
+                        <div className='count'>{`#${index + 1}`}</div>
+                        <div className='song'>
+                            <div className='imgBox'>
+                                <img src={song?.imgSrc} alt="" />
+                            </div>
 
-                          <div className='hits'>
-                              <p className='hit'>
-                                  <i><FaHeadphones /></i>
-                                  95,490,102
-                              </p>
+                            <div className='section'>
+                                <p className='songName'>
+                                    Take me on <span className='spanArtist'>
+                                        Artist Name
+                                    </span>
+                                </p>
 
-                              <p className='duration'>
-                                  <i><FaRegClock /></i>
-                                  03:04
-                              </p>
+                                <div className='hits'>
+                                    <p className='hit'>
+                                        <i><FaHeadphones /></i>
+                                        95,490,102
+                                    </p>
 
-                              <div className='favourite'>
-                                  <i><FaHeart /></i>
-                                  <i><FaRegHeart /></i>
-                              </div>
-                          </div>
-                      </div>
-                      </div> 
-                </div>
-            </div>
-         </div>;
+                                    <p className='duration'>
+                                        <i><FaRegClock /></i>
+                                        03:04
+                                    </p>
+
+                                    <div className='favourite'>
+                                        <i><FaHeart /></i>
+                                        <i><FaRegHeart /></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+
+        </div>
+    </div>;
 }
 
-export {AudioList};
+export { AudioList };
